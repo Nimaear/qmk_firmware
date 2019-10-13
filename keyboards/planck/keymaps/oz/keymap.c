d3d060abd4fe38fdde315da04e847371af8a4407
Oxygen <oz@trioxygen.io>
2019-10-13 23:04:59 +0200

    Add unicode characters to the planck

--- keyboards/planck/keymaps/oz/keymap.c
+++ keyboards/planck/keymaps/oz/keymap.c
@@ -25,13 +25,74 @@ enum planck_layers {
   _RAISE,
   _FNLYR
 };
+enum unicode_names {
+    O_UML,
+    O_UML_CAP,
+    U_UML,
+    U_UML_CAP,
+    A_UML,
+    A_UML_CAP,
+    A_RING,
+    A_RING_CAP,
+    BX_CBL, // ╰ 0x2570
+    BX_CBR, // ╯ 0x256F
+    BX_CTL, // ╭ 0x256D
+    BX_CTR, // ╮ 0x256E
+    BX_HR, // ─ 0x2500
+    BX_VERT, // │ 0x2502
+    BX_TL,  // ├ 0x251C
+    BX_TR,  // ┤ 0x2524
+    BX_TT,  // ┬ 0x252C
+    BX_TB,  // ┴ 0x2534
+    BX_TX,  // ┼ 0x253C
+    CAT_HPY,  // 0x1F638 😸
+    CAT_JOY, // U+1F639
+    CAT_GRN, // U+1F63A
+    CAT_LOV, // U+1F63B
+    CAT_CRY, // U+1F63F
+    CAT_O, // U+1F640
+    CAT_POU, // U+1F63E
+};
 
+const uint32_t PROGMEM unicode_map[] = {
+    [U_UML]      = 0x00FC,
+    [U_UML_CAP]  = 0x00DC,
+    [O_UML]      = 0x00F6,
+    [O_UML_CAP]  = 0x00D6,
+    [A_UML]      = 0x00E4,
+    [A_UML_CAP]  = 0x00C4,
+    [A_RING]     = 0x00E5,
+    [A_RING_CAP] = 0x00C5,
+    [BX_CBL]     = 0x2570,
+    [BX_CBR]     = 0x256F,
+    [BX_CTL]     = 0x256D,
+    [BX_CTR]     = 0x256E,
+    [BX_HR]      = 0x2500,
+    [BX_VERT]    = 0x2502,
+    [BX_TL]      = 0x251C,
+    [BX_TR]      = 0x2524,
+    [BX_TT]      = 0x252C,
+    [BX_TB]      = 0x2534,
+    [BX_TX]      = 0x253C,
+    [CAT_HPY]    = 0x1F638,
+    [CAT_JOY]    = 0x1F639,
+    [CAT_GRN]    = 0x1F63A,
+    [CAT_LOV]    = 0x1F63B,
+    [CAT_CRY]    = 0x1F63F,
+    [CAT_O]      = 0x1F640,
+    [CAT_POU]    = 0x1F63E,
+};
+
+#define KC_A_RN XP(A_RING, A_RING_CAP)
+#define KC_A_UML XP(A_UML, A_UML_CAP)
+#define KC_U_UML XP(U_UML, U_UML_CAP)
+#define KC_O_UML XP(O_UML, O_UML_CAP)
 /*
 sudo env CFLAGS="-Wno-error=deprecated" make planck/rev6:oz:dfu-util
 */
 
 enum planck_keycodes {
-    COLEMAK = SAFE_RANGE
+    COLEMAK = SAFE_RANGE,
 };
 
 #define LOWER MO(_LOWER)
@@ -53,15 +114,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, DEL_WRD,
     KC_DEL,  _______, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN,  KC_RCBR, KC_RBRC, _______, KC_PIPE,
-    _______, _______, _______, _______, KC_LCBR, KC_UNDS, KC_PLUS, KC_RCBR,  _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, _______,  _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    DEL_WRD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     _______,
+    DEL_WRD, KC_A_UML, _______, _______, _______, _______, _______, _______, KC_U_UML, _______, KC_O_UML, KC_BSLS,
+    _______, KC_A_RN,  _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______,
+    UC_M_LN, UC_M_WI,  _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______
 ),
 
 [_FNLYR] = LAYOUT_planck_grid(
